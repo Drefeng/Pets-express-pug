877711d00c46c9f33196b44737ce15e7e2925bbe
Billy Infante <infbil15@gmail.com>
2019-03-12 11:25:38 +0100

    hehe xd

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const petData = require('../data/pet.json')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,18 +10,19 @@ router.get('/', function(req, res, next) {
 
 // JSON PET DETAILS
 router.get('/data', function(req, res, next) {
-  res.render('data', { title: 'data' });
+  res.json(petData);
 });
 
 
 // INFO
 router.get('/info', function(req, res, next) {
-  res.render('info', { title: 'dogo' });
+  res.render('info',  {data: petData});
 });
 
 // PET PICTURES
 router.get('/pictures', function(req, res, next) {
-  res.render('picture', { title: 'pets' });
+
+  res.render('picture', );
 });
 
 
